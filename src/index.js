dcb665a816f44df646f817e30804a0c8aadf2698
fregnani123 <fabianofregnani@hotmail.com>
2023-05-24 18:52:05 -0300

    atualizar

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+class Pessoa {
+    constructor(nome, sobrenome){
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    }
+};
+
+const novaPessoa = new Pessoa('Fabiano','Fregnani');
+
+console.log(novaPessoa)
