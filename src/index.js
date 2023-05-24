3d1b44119720e017071c3be98f20d7cf3ef054fa
fregnani123 <fabianofregnani@hotmail.com>
2023-05-24 19:03:01 -0300

    estudos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 
-class Pessoa {
-    constructor(nome, sobrenome){
-    this.nome = nome;
-    this.sobrenome = sobrenome;
-    }
-};
+// class Pessoa {
+//     constructor(nome, sobrenome){
+//     this.nome = nome;
+//     this.sobrenome = sobrenome;
+//     }
+// };
 
-const novaPessoa = new Pessoa('Fabiano','Fregnani');
+// const novaPessoa = new Pessoa('Fabiano','Fregnani');
 
-console.log(novaPessoa)
+// console.log(novaPessoa)
+
+
+const nomes = ['Fabiano','Fernanda','Ada','Pedro']
+
+for(let i = 0; i < nomes.length; i++){
+    console.log(nomes[i])
+}
+
+for(pessoas of nomes){
+    console.log(pessoas)
+}
